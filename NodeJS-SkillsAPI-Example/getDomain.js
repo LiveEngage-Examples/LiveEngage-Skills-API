7ae44ff8bef125ebd2d5d249504b754715058fc7
Scott Westover <scottwestover2006@gmail.com>
2017-03-16 10:49:37 -0400

    Added example for getting the domain and updated read.me
    I updated the README.md file to let the user know for the node examples
    they need to install request. I added an example for getting the domain.

---
+++ NodeJS-SkillsAPI-Example/getDomain.js
@@ -0,0 +1,36 @@
+/*
+ * This is an example on how you can find you base domain for the LiveEngage Skill API. To run this example
+ * you will need to install request, and you will need to update the URL below with your LiveEngage account number.
+ */
+
+// run -  npm install request
+var request = require('request');
+
+/* The URL below is for the Read Only URL */
+var url = "https://api.liveperson.net/api/account/{YOUR ACCOUNT NUMBER}/service/accountConfigReadOnly/baseURI.json?version=1.0";
+/* The URL below is for the Read/Write URL */
+// var url = "https://api.liveperson.net/api/account/{YOUR ACCOUNT NUMBER}/service/accountConfigReadWrite/baseURI.json?version=1.0";
+
+request.get({
+    url: url,
+    json: true,
+    headers: {
+        'Content-Type': 'application/json'
+    }
+}, function (e, r, b) {
+    if (!e && r.statusCode == 200) {
+        console.log(JSON.stringify(b));
+    }
+    else {
+        console.log(e);
+    }
+});
+
+/*
+  Example Response:
+	{
+	 "service":"accountConfigReadOnly",
+	 "account":"56072331",
+	 "baseURI":"va-a.acr.liveperson.net"
+  }
+*/
--- README.md
+++ README.md
@@ -10,6 +10,12 @@ Thank you [David Eleff](https://github.com/deleff) for providing this example co
 ## NodeJS - Skills API Example
 This is a basic example of how to connect to the Skills API in NodeJS by using the request module. The example shows how you can get all of the skills that are on your account, how to get more information for a single skill by using their id, and how to create a skill using the API. In order to run this example you will need to update the js file with your Skills API credentials.
 
+In order to run these examples, you will need to install the request module. To install the module just run:
+
+```sh
+npm install request
+```
+
 ## Python - Skills API Sample
 This is a basic example of how to connect to the Skills API in Python by using the requests_oauthlib library. The example shows how you can get all of the skills that are on your account, and how to get more information for a single skill by using their id. In order to run this example you will need to update the SkillsAPI_Sample.py file with your Skills API credentials.
 
